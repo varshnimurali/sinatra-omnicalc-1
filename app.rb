92723b374ef4c40ff07f86c4bab829fcbb318f06
varshnimurali <vmurali0@chicagobooth.edu>
2025-04-21 18:13:06 +0000

    commit updated changes

--- app.rb
+++ app.rb
@@ -16,8 +16,19 @@ end
 get '/square/results' do
   @the_num = params.fetch("users_number").to_f
   @the_result = @the_num ** 2
-  
+
   erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root_calc)
+end
+
+get '/square_root/results' do
+  @the_num_2 = params.fetch("users_number_2").to_f
+  @the_result_2 = @the_num_2 ** 0.5
+  
+  erb(:square_root_results)
 end
 
 get("/") do
